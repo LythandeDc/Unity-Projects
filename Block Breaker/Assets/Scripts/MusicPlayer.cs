19a2f7dd9809e8f513df51b0ea2b4a1fd219c974
Ramon Boza <ramon.boza@king.com>
2015-03-18 15:17:54 +0100

    First commit with all the assets

---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer INSTANCE = null;
+
+	void Awake () {
+		Debug.Log ("Music player Awake " + GetInstanceID());
+		if(INSTANCE != null){
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		}else {
+			INSTANCE = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Music player Start " + GetInstanceID());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
